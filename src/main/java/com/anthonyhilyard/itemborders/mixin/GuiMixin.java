94c229471c4a2bbd1671fbf355bed551730ce074
Anthony Hilyard <anthony.hilyard@gmail.com>
2021-12-28 18:04:28 -0800

    Fixed caching issue causing slowdown. Added new visual options.

--- src/main/java/com/anthonyhilyard/itemborders/mixin/GuiMixin.java
+++ src/main/java/com/anthonyhilyard/itemborders/mixin/GuiMixin.java
@@ -6,6 +6,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -18,7 +19,7 @@ import net.minecraft.world.item.ItemStack;
 public class GuiMixin extends GuiComponent
 {
 	@Inject(method = "renderSlot(IIFLnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;I)V",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderAndDecorateItem(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;III)V"),
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderAndDecorateItem(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;III)V", shift = Shift.AFTER),
 			locals = LocalCapture.CAPTURE_FAILEXCEPTION)
 	public void renderSlot(int x, int y, float time, Player player, ItemStack item, int something, CallbackInfo info, PoseStack poseStack)
 	{
