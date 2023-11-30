4476cad2c8c94b134825da7183aecb2bba0c173d
Anthony Hilyard <anthony.hilyard@gmail.com>
2023-11-30 12:30:46 -0800

    Ported to 1.20.

--- src/main/java/com/anthonyhilyard/itemborders/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/com/anthonyhilyard/itemborders/mixin/AbstractContainerScreenMixin.java
@@ -1,7 +1,6 @@
 package com.anthonyhilyard.itemborders.mixin;
 
 import com.anthonyhilyard.itemborders.ItemBorders;
-import com.mojang.blaze3d.vertex.PoseStack;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.inventory.Slot;
@@ -20,9 +20,9 @@ public class AbstractContainerScreenMixin extends Screen
 	protected AbstractContainerScreenMixin(Component titleIn) { super(titleIn); }
 
 	@Inject(method = "renderSlot", at = @At(value = "INVOKE",
-			target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderAndDecorateItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;III)V", shift = Shift.AFTER))
-	public void renderSlot(PoseStack poseStack, Slot slot, CallbackInfo info)
+			target = "Lnet/minecraft/client/gui/GuiGraphics;renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", shift = Shift.AFTER))
+	public void renderSlot(GuiGraphics graphics, Slot slot, CallbackInfo info)
 	{
-		ItemBorders.renderBorder(poseStack, slot);
+		ItemBorders.renderBorder(graphics.pose(), slot);
 	}
 }
