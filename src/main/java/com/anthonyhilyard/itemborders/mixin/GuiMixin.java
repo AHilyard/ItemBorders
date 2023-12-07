edf2720b094d83e0ad35aac24d911280a58332c2
Anthony Hilyard <anthony.hilyard@gmail.com>
2023-12-07 13:10:32 -0800

    Ported to 1.20.

--- src/main/java/com/anthonyhilyard/itemborders/mixin/GuiMixin.java
+++ src/main/java/com/anthonyhilyard/itemborders/mixin/GuiMixin.java
@@ -1,7 +1,6 @@
 package com.anthonyhilyard.itemborders.mixin;
 
 import com.anthonyhilyard.itemborders.ItemBorders;
-import com.mojang.blaze3d.vertex.PoseStack;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,18 +8,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
 @Mixin(Gui.class)
-public class GuiMixin extends GuiComponent
+public class GuiMixin
 {
 	@Inject(method = "renderSlot",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;renderAndDecorateItem(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;III)V", shift = Shift.AFTER))
-	public void renderSlot(PoseStack poseStack, int x, int y, float time, Player player, ItemStack item, int something, CallbackInfo info)
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;II)V", shift = Shift.AFTER))
+	public void renderSlot(GuiGraphics graphics, int x, int y, float time, Player player, ItemStack item, int something, CallbackInfo info)
 	{
-		ItemBorders.renderBorder(poseStack, item, x, y);
+		ItemBorders.renderBorder(graphics.pose(), item, x, y);
 	}
 }
