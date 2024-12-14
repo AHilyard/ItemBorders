615b03167908a5c4d1b835e0640e42a97fad6a15
Anthony Hilyard <anthony.hilyard@gmail.com>
2024-12-14 12:36:20 -0800

    Fixed Forge loading issue.

--- forge/src/main/java/com/anthonyhilyard/itemborders/forge/ItemBordersForge.java
+++ forge/src/main/java/com/anthonyhilyard/itemborders/forge/ItemBordersForge.java
@@ -3,13 +3,13 @@ package com.anthonyhilyard.itemborders.forge;
 import com.anthonyhilyard.itemborders.ItemBorders;
 
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ItemBorders.MODID)
 public final class ItemBordersForge
 {
-	public ItemBordersForge(ModLoadingContext context)
+	public ItemBordersForge(FMLJavaModLoadingContext context)
 	{
 		context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "ANY", (remote, isServer) -> true));
 	}
